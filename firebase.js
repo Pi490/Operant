eaf536b3bbe848f495ca1f2d9f9af9723ccfab51
sistema <alvescellipiettra@gmail.com>
2026-06-12 13:29:40 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,51 @@
-ç
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  getDoc,
+  collection,
+  query,
+  where,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDrVJWdFnAljROeBYQEWcyAwxpLUI7TPDw",
+    authDomain: "gestao-ferramentas-fd5b7.firebaseapp.com",
+    projectId: "gestao-ferramentas-fd5b7",
+    storageBucket: "gestao-ferramentas-fd5b7.firebasestorage.app",
+    messagingSenderId: "953729249758",
+    appId: "1:953729249758:web:2f820c75e0a08d24ea2d4f",
+    measurementId: "G-1JEQ3DM0HL"
+  };
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+
+export {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  doc,
+  setDoc,
+  getDoc,
+  collection,
+  query,
+  where,
+  getDocs
+};
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
+
+export const storage = getStorage(app);
