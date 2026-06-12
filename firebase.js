e090422f94847c79e3fb6dbdf51dc28ab03d5bd9
sistema <alvescellipiettra@gmail.com>
2026-06-12 13:28:07 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -19,13 +19,14 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCbfK38PLVK9APVyEsxR_XWNZ7_Ef22qRQ",
-  authDomain: "gestao-de-ferramentas-teste.firebaseapp.com",
-  projectId: "gestao-de-ferramentas-teste",
-  storageBucket: "gestao-de-ferramentas-teste.appspot.com",
-  messagingSenderId: "279635050715",
-  appId: "1:279635050715:web:93b490b68cf91ffaf10cfe"
-};
+    apiKey: "AIzaSyDrVJWdFnAljROeBYQEWcyAwxpLUI7TPDw",
+    authDomain: "gestao-ferramentas-fd5b7.firebaseapp.com",
+    projectId: "gestao-ferramentas-fd5b7",
+    storageBucket: "gestao-ferramentas-fd5b7.firebasestorage.app",
+    messagingSenderId: "953729249758",
+    appId: "1:953729249758:web:2f820c75e0a08d24ea2d4f",
+    measurementId: "G-1JEQ3DM0HL"
+  };
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
